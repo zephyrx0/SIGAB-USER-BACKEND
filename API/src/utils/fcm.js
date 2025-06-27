b6d13c6f8b1dbd6d736b8f2bf6c1163caf1fdf1e
zephyrx0 <ganelajeisa@gmail.com>
2025-06-28 03:17:01 +0700

    hilangkan double log

--- API/src/utils/fcm.js
+++ API/src/utils/fcm.js
@@ -62,14 +62,12 @@ async function sendFcmTopicNotification(topic, title, body, data = {}) {
     },
   };
 
-  console.log('[FCM] Akan mengirim notifikasi ke topic:', topic, title, body);
   const response = await axios.post(url, message, {
     headers: {
       'Authorization': `Bearer ${accessToken}`,
       'Content-Type': 'application/json',
     },
   });
-  console.log('[FCM] Selesai kirim notifikasi ke topic:', topic);
 
   return response.data;
 }
